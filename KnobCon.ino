af2918a9d9b0d241dd8abea8bc85342ba00b9c0b
kyontan <kyontan@monora.me>
2016-07-30 18:29:50 +0900

    Initial commit

---
+++ KnobCon.ino
@@ -0,0 +1,143 @@
+#define USBCON
+
+#include <Joystick.h>
+
+#define PIN_LED 17
+#define PIN_A 2
+#define PIN_B 3
+#define PIN_SW 7
+
+#define CHATTERING_THRESHOLD_TIME_MS 100
+
+//  CW: Clock-wise
+// CCW: Counter-clock-wise
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+
+	pinMode(PIN_A, INPUT);
+	pinMode(PIN_B, INPUT);
+
+	digitalWrite(PIN_A, HIGH);
+	digitalWrite(PIN_B, HIGH);
+
+// pin | interrupt no
+//  3  | 0
+//  2  | 1
+//  0  | 2
+//  1  | 3
+//  7  | 4
+	attachInterrupt(1, a_change, CHANGE); // pin2: A
+	attachInterrupt(0, b_change, CHANGE); // pin3: B
+	attachInterrupt(4, switch_change, CHANGE);  // pin7: Switch
+
+  // Serial.begin(115200);
+	Joystick.begin();
+}
+
+volatile int state = LOW;
+volatile int last_a = 0;
+volatile int last_b = 0;
+
+// joystick
+volatile int rotation = 0;
+
+void loop() {
+  digitalWrite(LED, state);
+}
+
+void rotation_update(int dir) {
+	rotation += 3.75 * dir; // 360 / (24 * 4)
+	rotation = fmod(360 + rotation, 360); // (360 + rotation) % 360 => 0 <= rotation < 360
+	// Serial.println(rotation, DEC);
+	Joystick.setXAxisRotation((int)rotation);
+
+	if (dir == 1) { // CW
+		state = 1;
+	} else { // CCW
+		state = 0;
+	}
+}
+
+volatile int last_update_mills = 0;
+bool is_chattering() {
+	if ((last_update_mills - millis()) < CHATTERING_THRESHOLD_TIME_MS) {
+		return 1;
+	}
+	last_update_mills = millis();
+	return 0;
+}
+
+// volatile int update_count = 0;
+// bool is_chattering() {
+// 	if (10 < update_count++) {
+// 		update_count = 0;
+// 		return 0;
+// 	}
+// 	return 1;
+// }
+
+void a_change() {
+	if (is_chattering()) { return; }
+
+	int current_a = digitalRead(PIN_A);
+	if (last_a == current_a) { return; }
+
+	last_b = digitalRead(PIN_B);
+	int dir = 0;
+
+	if (last_a < current_a) { // rising
+		if (last_b) {
+			rotation_update(1); // CW
+			// Serial.println("A:R:CW");
+		} else {
+			// Serial.println("A:R:CCW");
+			rotation_update(-1); // CCW
+		}
+	} else { // falling
+		if (last_b) {
+			rotation_update(1); // CW
+			// Serial.println("A:F:CCW");
+		} else {
+			rotation_update(-1); // CCW
+			// Serial.println("A:F:CW");
+		}
+	}
+	last_a = current_a;
+}
+
+void b_change() {
+	if (is_chattering()) { return; }
+
+	int current_b = digitalRead(PIN_B);
+	if (last_b == current_b) { return; }
+
+	last_a = digitalRead(PIN_A);
+	int dir = 0;
+
+	if (last_b < current_b) { // rising
+		if (last_a) {
+			rotation_update(-1); // CCW
+			// Serial.println("B:R:CCW");
+		} else {
+			rotation_update(1); // CW
+			// Serial.println("B:R:CW");
+		}
+	} else { // falling
+		if (last_a) {
+			rotation_update(1); // CW
+			// Serial.println("B:F:CW");
+		} else {
+			rotation_update(-1); // CCW
+			// Serial.println("B:F:CCW");
+		}
+	}
+	last_b = current_b;
+}
+
+void switch_change() {
+	int current_sw = digitalRead(PIN_SW);
+	// Serial.println(current_sw);
+	Joystick.setButton(0, current_sw);
+  // state = !state;
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# About
+
+[豆腐屋](http://tofu-ya.moe/) が製作したノブ型コントローラー KnobCon の制御ソフトウェアです
+
+## 3Dモデル, 回路図等
+
+[KnobCon 公式サイト](http://tofu-ya.moe/knobcon) にて配布しています
+
+## 使用ライブラリ
+
+開発には Arduino を使用しています。ご使用の OS に対応した Arduino を [公式サイト](https://www.arduino.cc) よりダウンロードし、インストールしてください。
+
+### Arduino Joystick Library
+
+[MHeironimus/ArduinoJoystickLibrary](https://github.com/MHeironimus/ArduinoJoystickLibrary/tree/master/)
+
+上記リンクの Download Zip (もしくは[ここ](https://github.com/MHeironimus/ArduinoJoystickLibrary/archive/master.zip)) よりダウンロードしてください。
+ダウンロードして展開した後、以下のディレクトリ内に JoyStick ディレクトリを丸ごと(フォルダごと)移動してください
+
+- Windows
+  - `ドキュメント` フォルダ内の、 `Arduino/libraries` 内
+- macOS (OSX)
+  - `書類` ディレクトリ内の、 `Arduino/libraries` 内
+  - `~/Documents/Arduino/libraries`
