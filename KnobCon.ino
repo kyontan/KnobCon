064d299e69635983092bfc13a7fbd85b1db5af25
kyontan <kyontan@monora.me>
2016-08-03 02:09:27 +0900

    Implement keyboard mode and mode selecting

--- KnobCon.ino
+++ KnobCon.ino
@@ -1,6 +1,7 @@
 #define USBCON
 
 #include <Joystick.h>
+#include <Keyboard.h>
 
 #define PIN_LED 17
 #define PIN_A 2
@@ -12,6 +13,8 @@
 //  CW: Clock-wise
 // CCW: Counter-clock-wise
 
+bool mode_joystick = false;
+bool mode_keyboard = false;
 const bool enable_serial = false; // debug
 
 void setup() {
@@ -19,9 +22,14 @@ void setup() {
 
 	pinMode(PIN_A, INPUT);
 	pinMode(PIN_B, INPUT);
+	pinMode(PIN_SW, INPUT);
 
 	digitalWrite(PIN_A, HIGH);
 	digitalWrite(PIN_B, HIGH);
+
+  // 起動時にスイッチを押していなければキーボードになる
+	mode_joystick = digitalRead(PIN_SW);
+	mode_keyboard = !mode_joystick;
 
 // pin | interrupt no
 //  3  | 0
@@ -36,7 +44,15 @@ void setup() {
   if (enable_serial) {
   	Serial.begin(115200);
   	while (!Serial);
+
+  	Serial.print("mode_joystick: ");
+  	Serial.println(mode_joystick);
+  	Serial.print("mode_keyboard: ");
+  	Serial.println(mode_keyboard);
   }
+
+	if (mode_joystick) Joystick.begin();
+	if (mode_keyboard) Keyboard.begin();
 }
 
 volatile int last_a = 0;
@@ -52,11 +68,13 @@ void loop() {
 void rotation_update(int dir) {
 	rotation += 3.75 * dir; // 360 / (24 * 4)
 	rotation = fmod(360 + rotation, 360); // (360 + rotation) % 360 => 0 <= rotation < 360
-	Joystick.setXAxisRotation((int)rotation);
 	if (enable_serial) Serial.println(rotation, DEC);
+	if (mode_joystick) Joystick.setXAxisRotation((int)rotation);
 
 	if (dir == 1) { // CW
+		Keyboard.write('>');
 	} else { // CCW
+		Keyboard.write('<');
 	}
 }
 
@@ -134,6 +152,13 @@ void b_change() {
 
 void switch_change() {
 	int current_sw = digitalRead(PIN_SW);
-	Joystick.setButton(0, current_sw);
 	if (enable_serial) Serial.println(current_sw);
+	if (mode_joystick) Joystick.setButton(0, current_sw);
+	if (mode_keyboard) {
+		if (current_sw) {
+			Keyboard.press(KEY_RETURN);
+		} else {
+			Keyboard.release(KEY_RETURN);
+		}
+	}
 }
